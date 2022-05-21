333dbd1d7a7b4dfd06c097fa1804e7c48fe983ea
adityanurali <69146268+adityanurali@users.noreply.github.com>
2022-05-21 22:59:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const list = document.querySelectorAll('.list')
+const itemBox = document.querySelectorAll('.item-box')
+
+list.forEach((elem)=>{
+    elem.addEventListener('click',function(){
+        list.forEach((elem)=>{
+            elem.classList.remove('active');
+        });
+        this.classList.add('active');
+
+        let value = this.getAttribute('data-filter');
+        itemBox.forEach(element=>{
+            element.classList.add('hide');
+            if(element.getAttribute('data-item') == value || value == "all"){
+                element.classList.remove('hide');
+            }
+        })
+
+    })
+
+})
